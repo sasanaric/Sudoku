1aab201b70924f48fe0e07d327caf7efd047f4e3
snaric <snaric01@gmail.com>
2023-04-08 16:01:33 +0200

    1

---
+++ SudokuApp/SudokuApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace SudokuApp
+{
+    class Sudoku
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLOU");
+        }
+    }
+}
