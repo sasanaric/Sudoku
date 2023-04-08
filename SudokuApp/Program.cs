09dcb7cbb81a24f438568afc32eb6af5bd2d2994
snaric <snaric01@gmail.com>
2023-04-08 15:26:09 +0200

    2

--- SudokuApp/Program.cs
+++ SudokuApp/Program.cs
@@ -1,3 +1,18 @@
 ﻿// See https://aka.ms/new-console-template for more information
-
-Console.WriteLine("Hello, World!");
+using System;
+using System.Collections.Generic;
+class Sudoku
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        List<int> niz = new List<int>();
+        niz.Add(1);
+        niz.Add(2);
+        Console.WriteLine(niz);
+        foreach (int i in niz)
+        {
+            Console.WriteLine(i);
+        }
+    }
+}
