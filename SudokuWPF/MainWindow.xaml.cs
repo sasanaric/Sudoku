fdcae0718fd7e53822255dfb2574d4f5bdab58e7
snaric <snaric01@gmail.com>
2023-04-09 14:10:00 +0200

    mm

--- SudokuWPF/MainWindow.xaml.cs
+++ SudokuWPF/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace SudokuWPF
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SudokuWPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -8,6 +12,23 @@
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void OnCellClick(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = sender as TextBox;
+
+            // Check if the TextBox is not null
+            if (textBox != null)
+            {
+                // Set the text of the TextBox to some value, e.g. "1"
+                textBox.Text = "1";
+            }
+        }
+
+        private void TextBoxKeyboard(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
         }
     }
 }
